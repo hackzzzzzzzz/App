134ea9cebf334b6af0f11141796c93781e264c52
Anand <venkatanand15@gmail.com>
2022-12-19 12:43:30 +0530

    changess!

--- app/src/main/java/com/example/login/scheme.java
+++ app/src/main/java/com/example/login/scheme.java
@@ -1,13 +1,88 @@
 package com.example.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
+
 
 
 public class scheme extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schemes);
+
+
+//------------------------------------------------------------------------------------------------
+        drawerLayout = findViewById(R.id.Drawer);
+        navigationView = findViewById(R.id.Navigation);
+        toolbar = findViewById(R.id.include);
+
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_baseline_dehaze_24);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem v) {
+                switch (v.getItemId()) {
+                    case R.id.home:
+                        Intent  intent = new Intent(scheme.this, homepage.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.calen:
+                       intent = new Intent(scheme.this, calender.class);
+                        startActivity(intent);
+                        break;
+
+                    case R.id.about:
+                        Toast.makeText(scheme.this, "about is clicked", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.feedback:
+                        Toast.makeText(scheme.this, "feedback is clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.logout:
+                        intent = new Intent(scheme.this, MainActivity.class);
+                        startActivity(intent);
+                        break;
+
+                }
+                return true;
+            }
+        });
+
+
     }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
