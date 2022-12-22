de0d8d6c8dd8ee7e13a938b489e72a4ffd8c1d9c
Anand <venkatanand15@gmail.com>
2022-12-22 20:12:07 +0530

    changess!

---
+++ app/src/main/java/com/example/login/Forgetpass.java
@@ -0,0 +1,180 @@
+package com.example.login;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.TaskExecutors;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.AuthProvider;
+import java.util.concurrent.TimeUnit;
+
+
+public class Forgetpass extends AppCompatActivity {
+
+    String codebysystem;
+    TextView textView;
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+    FirebaseAuth auth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.forget_pass);
+
+        EditText phone = (EditText) findViewById(R.id.phonenum);
+        textView = (TextView) findViewById(R.id.otp);
+        EditText enterotp = (EditText) findViewById(R.id.enterotp);
+        MaterialButton confirm = (MaterialButton) findViewById(R.id.confirmbutton);
+        auth = FirebaseAuth.getInstance();
+        FirebaseAuth firebaseAuth;
+
+
+
+
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String phonenumm = phone.getText().toString();
+
+
+                if(phonenumm.isEmpty() || phonenumm.length()<10){
+                    phone.setError("Enter Correct Number");
+                }
+                else{
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            if(snapshot.hasChild(phonenumm)){
+                                Toast.makeText(Forgetpass.this, "otp sent", Toast.LENGTH_SHORT).show();
+                                sendotp();
+
+                            }
+                            else{
+                                Toast.makeText(Forgetpass.this, "User Not Found", Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+
+                        private void sendotp() {
+                            PhoneAuthProvider.verifyPhoneNumber(
+                                    PhoneAuthOptions.newBuilder(FirebaseAuth.getInstance())
+                                            .setActivity(Forgetpass.this)
+                                            .setPhoneNumber(phonenumm)
+                                            .setTimeout(60L,TimeUnit.SECONDS)
+                                            .setCallbacks(mcallbacks)
+                                            .build());
+                        }
+
+                        private PhoneAuthProvider.OnVerificationStateChangedCallbacks mcallbacks=
+                                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+                                    @Override
+                                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                        super.onCodeSent(s, forceResendingToken);
+                                        codebysystem = s;
+
+                                    }
+
+                                    @Override
+                                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                        String code = phoneAuthCredential.getSmsCode();
+                                        if(code!=null){
+                                            enterotp.setText(code);
+                                            verifycode(code);
+
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                                        Toast.makeText(Forgetpass.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                };
+
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+                }
+
+
+
+
+            }
+        });
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = enterotp.getText().toString();
+                if(!code.isEmpty()){
+                    verifycode(code);
+                }
+            }
+        });
+
+    }
+
+    private void verifycode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codebysystem,code);
+        FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        openpasschange(credential);
+                    }
+                    else {
+                        Toast.makeText(Forgetpass.this, "Wrong OTP", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
+    }
+
+
+
+    private void openpasschange(PhoneAuthCredential credential) {
+        Intent intent = new Intent(Forgetpass.this , passchange.class);
+        startActivity(intent);
+        finish();
+    }
+
+
+}
---
+++ app/src/main/java/com/example/login/passchange.java
@@ -0,0 +1,73 @@
+package com.example.login;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.EmailAuthCredential;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class passchange extends AppCompatActivity {
+    DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pass_change);
+
+        EditText phonenumber = (EditText) findViewById(R.id.phonenum);
+        EditText newpass = (EditText) findViewById(R.id.newpass);
+        EditText confirmnewpass = (EditText) findViewById(R.id.confirmnewpass);
+        MaterialButton confirm = (MaterialButton) findViewById(R.id.updateconfirm);
+
+
+
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final String phone = phonenumber.getText().toString();
+                final String pass1 = newpass.getText().toString();
+                final String pass2 = confirmnewpass.getText().toString();
+
+
+                if(pass1.isEmpty() || pass1.length()<8 || pass2.isEmpty() || pass2.length()<8){
+                    newpass.setError("Passowrd too Short Minimum 8 characters");
+                }
+                else if(!pass1.equals(pass2))
+                {
+                    Toast.makeText(passchange.this, "Passwords Dosen't Match", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+                    reference.child(phone).child("Password").setValue(pass2);
+                    startActivity(new Intent(getApplicationContext(),Login.class));
+                    finish();
+                }
+
+            }
+        });
+
+
+      }
+}
